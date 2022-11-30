9ec1e3ce088158bb9f02c53c86154edf29a815f5
Ali Eren <110503291+AliEren04@users.noreply.github.com>
2022-11-30 23:44:15 +0000

    Update index.js

--- index.js
+++ index.js
@@ -1,12 +1,16 @@
 //Installed node-port-scanner package and used and defining it here
 const portScanner = require("node-port-scanner");
+//Defining Prompt Package
+const prompt = require('prompt-sync')();
+
+const someSpace = " "; 
 
 //PortScanner Welcomer
 console.log(
-  "Welcome to an Another Simple PortScanner Written With Node and by AliEren04"
+  "[Welcome to an Another Simple PortScanner Written With Node and by AliEren04]"
 );
 console.log(
-  "This port scanner have capability of scanning local common and remote common ports and also all ports available in local"
+  "[This port scanner have capability of scanning local common and remote common ports and also all ports available in local]"
 );
 
 //All functions to scan local common to remote common and all local ports
@@ -21,7 +25,10 @@ const scanLocalCommon = () => {
 };
 
 const scanRemoteCommon = () => {
-  portScanner("www.github.com", [21, 22, 23, 25, 80, 110, 123, 443])
+
+  let hostUrl = prompt('[Enter the host address you want to scan for example, www.github.com]' + someSpace)
+
+  portScanner(hostUrl, [21, 22, 23, 25, 80, 110, 123, 443])
     .then((results) => {
       console.log(results);
     })
@@ -37,9 +44,9 @@ async function checkAllLocalPorts() {
 
 //making a question to ask user to select a scan type
 //Conditions to scan which port depends on user input
-console.log("Enter 1 to Scan common local ports");
-console.log("Enter 2 to Scan common remote ports but change url in index js to url you want to scan");
-console.log("Enter 3 to Scan All Local Ports Available");
+console.log("[Enter 1 to Scan common local ports]");
+console.log("[Enter 2 to Scan common remote ports]");
+console.log("[Enter 3 to Scan All Local Ports Available]");
 process.stdin.setEncoding("utf-8");
 let scanQuestion;
 process.stdin.on("readable", function () {
@@ -48,9 +55,9 @@ process.stdin.on("readable", function () {
     scanLocalCommon();
   } else if (scanQuestion == 2) {
     scanRemoteCommon();
-  } else if (scanQuestion == 3) {
+} else if (scanQuestion == 3) {
     checkAllLocalPorts();
-  }else{
+  } else if (scanQuestion == null) {
     process.exit(1);
   }
 });
